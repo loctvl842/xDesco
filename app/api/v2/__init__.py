1779632a4dbe05b42ecb9e48b398230913667e91
loctvl842 <loclepnvx@gmail.com>
2024-05-06 21:47:11 +0700

    :bug: Replace v2

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.disease import router as disease_router
+from app.api.v2.disease import router as disease_router
 
 router = APIRouter(prefix="/v2")
 router.include_router(disease_router)
